31b8c5de9f7e833357e902b7be67c01190f2c84a
magdysia <magdysia@gmail.com>
2026-04-02 17:30:50 +0200

    feat: introduce Firebase env separation (dev/prod config split)

---
+++ src/config/firebase/firebase.prod.ts
@@ -0,0 +1,8 @@
+export const firebaseProdConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY_PROD,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN_PROD,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID_PROD,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET_PROD,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID_PROD,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID_PROD,
+};
