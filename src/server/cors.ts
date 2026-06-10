572061a2cbc67ed88befeaf35f2f495dff3b1a0d
magdysia <magdysia@gmail.com>
2026-06-10 15:16:00 +0200

    fix: allow no-cache request headers in cors

--- src/server/cors.ts
+++ src/server/cors.ts
@@ -6,7 +6,10 @@ export function setCorsHeaders(
 ) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", methods);
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, Content-Type, Cache-Control, Pragma"
+  );
   res.setHeader("Cache-Control", "no-store, no-cache, max-age=0, must-revalidate");
   res.setHeader("CDN-Cache-Control", "no-store");
   res.setHeader("Vercel-CDN-Cache-Control", "no-store");
